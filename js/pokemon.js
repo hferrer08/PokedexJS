9e82080adc707fe0ebb9cafe0405e52793f35cba
Hubert Ferrer Guerrero <hubert.ferrerg8@gmail.com>
2022-06-29 07:49:19 -0400

    Se cambia ruta de fetch

--- js/pokemon.js
+++ js/pokemon.js
@@ -23,28 +23,7 @@ class Pokemon{
     
     }
     
-    //Creacion de objetos
-    
-    const pokemon1 = new Pokemon('001',"bulbasaur","planta","hoja afilada",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png','https://www.cpokemon.com/images/pokedex/sprites/dp/001-f.png','#2CC61A')
-    const pokemon2 = new Pokemon('002',"ivysaur","planta","hoja afilada",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png','https://www.cpokemon.com/images/pokedex/sprites/dp/002-f.png','#2CC61A')
-    const pokemon3 = new Pokemon('003',"venusaur","planta","rayo solar",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png','https://www.cpokemon.com/images/pokedex/sprites/dp/003-f.png','#2CC61A')
-    const pokemon4 = new Pokemon('004',"charmander","fuego","ascuas",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png','https://www.cpokemon.com/images/pokedex/sprites/dp/004-f.png','#FFA500')
-    const pokemon5 = new Pokemon('005',"charmeleon","fuego","ascuas",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png','https://www.cpokemon.com/images/pokedex/sprites/dp/005-f.png','#FFA500')
-    const pokemon6 = new Pokemon('006',"charizard","fuego","lanzallamas",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png','https://www.cpokemon.com/images/pokedex/sprites/dp/006-m.png','#FFA500')
-    const pokemon7 = new Pokemon('007',"squirtle","agua","burbujas",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png','https://www.cpokemon.com/images/pokedex/sprites/dp/007-f.png','#1B4F72')
-    const pokemon8 = new Pokemon('008',"wartortle","agua","burbujas",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png','https://www.cpokemon.com/images/pokedex/sprites/dp/008-f.png','#1B4F72')
-    const pokemon9 = new Pokemon('009',"blastoise","agua","hidrobomba",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png','https://www.cpokemon.com/images/pokedex/sprites/dp/009-f.png','#1B4F72')
-    const pokemon10 = new Pokemon('010',"caterpie","bicho","placaje",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png','https://www.cpokemon.com/images/pokedex/sprites/dp/010-f.png','#6BC61A')
-    const pokemon11 = new Pokemon('011',"metapod","bicho","fortaleza",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/011.png','https://www.cpokemon.com/images/pokedex/sprites/dp/011-f.png','#6BC61A')
-    const pokemon12 = new Pokemon('012',"butterfree","bicho","confusion",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/012.png','https://www.cpokemon.com/images/pokedex/sprites/dp/012-f.png','#6BC61A')
-    const pokemon13 = new Pokemon('013',"weedle","bicho","picotazo venenoso",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/013.png','https://www.cpokemon.com/images/pokedex/sprites/dp/013-f.png','#6BC61A') 
-   const pokemon14 = new Pokemon('014',"kakuna","bicho","fortaleza",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/014.png','https://www.cpokemon.com/images/pokedex/sprites/dp/014-f.png','#6BC61A')
-   const pokemon15 = new Pokemon('015',"beedrill","bicho","placaje",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/015.png','https://www.cpokemon.com/images/pokedex/sprites/dp/015-f.png','#6BC61A')
-const pokemon16 = new Pokemon('016',"pidgey","volador","placaje",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png','https://www.cpokemon.com/images/pokedex/sprites/dp/016-f.png','#D6D2E7') 
-   const pokemon17 = new Pokemon('017',"pidgeotto","volador","placaje",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/017.png','https://www.cpokemon.com/images/pokedex/sprites/dp/017-f.png','#D6D2E7')
-   const pokemon18 = new Pokemon('018',"pidgeot","volador","placaje",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/018.png','https://www.cpokemon.com/images/pokedex/sprites/dp/018-f.png','#D6D2E7')
-  const pokemon19 = new Pokemon('019',"rattata","normal","placaje",1,'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/019.png','https://www.cpokemon.com/images/pokedex/sprites/dp/019-f.png','#D2B48C')
-    
+ 
     
     //Crear la array con los pokemon
     // const listaPokedex = [pokemon1,pokemon2,pokemon3,pokemon4,pokemon5,pokemon6,pokemon7,pokemon8,pokemon9,pokemon10,pokemon11,pokemon12,pokemon13,pokemon14,pokemon15,pokemon16,pokemon17,pokemon18,pokemon19]
